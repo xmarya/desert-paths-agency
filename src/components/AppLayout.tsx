db5fff53f464e74f6ef5fcd78ec8d7f7d4ba774c
xmarya <marya_hy@hotmail.com>
2025-10-18 14:13:29 +0300

    AppLayout

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,19 +1,18 @@
 import { Outlet } from "react-router-dom";
 import { styled } from "styled-components";
+import Header from "./Header";
 
 const Layout = styled.main`
   display: flex;
   flex-direction: column;
   gap: var(--section-vertical-space);
   margin: 0 auto;
-
-  border: var(--check);
 `;
 
 export default function AppLayout() {
   return (
     <>
-      {/* <Header/> */}
+      <Header/>
       <Layout>
         <Outlet />
       </Layout>
